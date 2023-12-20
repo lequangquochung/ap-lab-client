a0076481994ffb6192fc78005649f962a44d64a3
hunglqq <lequangquochung1912@gmail.com>
2023-12-20 21:46:49 +0700

    edit layout detail service

--- src/app/pages/review-lab/review-lab.component.ts
+++ src/app/pages/review-lab/review-lab.component.ts
@@ -35,37 +35,16 @@ export class ReviewLabComponent implements OnInit {
                 numScroll: 1
             }
         ];
-
-        this.products = [
-            // {
-            //     name: 'hung',
-            //     src: 'assets/img/caro/caro-3.png'
-            // },
-            // {
-            //     name: 'huz',
-            //     src: 'https://fl-img-media.s3.amazonaws.com/uploads/2020/11/Logo-only.png'
-            // },
-            // {
-            //     name: 'huz',
-            //     src: 'https://images.unsplash.com/photo-1508919801845-fc2ae1bc2a28?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fHww'
-            // },
-            // {
-            //     name: 'huz',
-            //     src: 'https://media.istockphoto.com/id/1210681509/fr/photo/les-mains-de-femme-placent-ensemble-comme-priant-devant-le-fond-de-ciel-de-coucher-du-soleil.jpg?s=170667a&w=0&k=20&c=xYmFB7HORtzk_Y9N3yuYwNOc9dp5wduG1DU9bRWzk0k='
-            // }
-        ]
     }
 
     getAllPhoto() {
         this.reviewLabService.getAllPhoto().subscribe({
             next: (res) => {
                 if(res.success) {
-                    
                     this.products = res.data.map((item:any)=>{
                         item.image = this.convertToImg(item.image);
                         return item;
                     })
-                    console.log(this.products);
                 }
             }
         })

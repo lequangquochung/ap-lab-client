b59d0e15bcfb6252603b817243256873f2ac6b89
hunglqq <lequangquochung1912@gmail.com>
2023-12-26 00:55:53 +0700

    update add background

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './service/dashboard.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
-  constructor() {}
+export class DashboardComponent implements OnInit {
+  constructor(private dashboardService: DashboardService,
+    private _sanitizer: DomSanitizer) { }
+
+  backgroundSrc: any;
+  defaultSrc: string = 'assets/img/bg/frame-1-resize.png';
+
+  ngOnInit(): void {
+    this.getPhoto();
+  }
+
+  getPhoto() {
+    this.dashboardService.getBackgroundImg().subscribe({
+      next: (res) => {
+        if (res.success) {
+          this.backgroundSrc = this.convertToImg(res?.data?.image)
+        } else {
+          this.backgroundSrc = this.defaultSrc;
+        }
+      },
+      error: () => {
+        this.backgroundSrc = this.defaultSrc;
+      }
+    })
+  }
+
+  convertToImg(stringBase: string): SafeUrl {
+    const imageUrl = stringBase;
+    return this._sanitizer.bypassSecurityTrustUrl(imageUrl);
+  }
+
+  handleImageError(e: Event) {
+    const imgElement = e.target as HTMLImageElement;
+    imgElement.src =  this.defaultSrc;
+}
+
 }
--- src/app/pages/dashboard/service/dashboard.service.ts
+++ src/app/pages/dashboard/service/dashboard.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
+import { environment } from "src/environments/environment.development";
 import { IResponseData } from "../../models/response-data.model";
 import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
   })
-export class ReviewLabService {
+export class DashboardService{
     apiBaseUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) { }
 
-    public getAllPhoto(): Observable<IResponseData<any>> {
-        return this.http.get<any>(`${this.apiBaseUrl}/lab-image`);
+    getBackgroundImg():  Observable<IResponseData<any>> {
+        return this.http.get<any>(`${this.apiBaseUrl}/lab-image/dashboard/background`);
     }
+    
 }

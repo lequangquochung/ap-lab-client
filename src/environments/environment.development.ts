2f306e853c2cd12c157b7b0e34eb6117bfcfeab6
hunglqq <lequangquochung1912@gmail.com>
2024-01-13 03:08:40 +0700

    s

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ap-lab-be-production.up.railway.app'
+  // apiUrl: 'https://ap-lab-be-production.up.railway.app'
+  apiUrl: 'https://pgdteam.tech'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ap-lab-be-production.up.railway.app'
+  // apiUrl: 'https://ap-lab-be-production.up.railway.app'
+  apiUrl: 'https://pgdteam.tech'
 };
  
